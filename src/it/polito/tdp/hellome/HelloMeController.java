991edd5a73807b1c88fcccdc61b926d7a5227af1
Lore <lorenzo.clericuzio@studenti.polito.it>
2018-03-21 23:31:42 +0100

    Versione iniziale programma

---
+++ src/it/polito/tdp/hellome/HelloMeController.java
@@ -0,0 +1,51 @@
+/**
+ * Sample Skeleton for 'HelloMe.fxml' Controller Class
+ */
+
+package it.polito.tdp.hellome;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class HelloMeController {
+
+    @FXML // ResourceBundle that was given to the FXMLLoader
+    private ResourceBundle resources;
+
+    @FXML // URL location of the FXML file that was given to the FXMLLoader
+    private URL location;
+
+    @FXML // fx:id="txtNome"
+    private TextField txtNome; // Value injected by FXMLLoader
+
+    @FXML // fx:id="txtResult"
+    private TextArea txtResult; // Value injected by FXMLLoader
+
+    @FXML
+    void handleCIao(ActionEvent event) {
+    	
+    	String nome= txtNome.getText();
+    	
+    	if(nome.length()==0)
+    		
+    		txtResult.setText("Errore, devi inserire un nome!");
+    	
+    	else{	
+    		
+    	String messaggio="Benvenuto, "+ nome+ "!\n";
+    	txtResult.setText(messaggio);
+    	
+    	}
+    }
+
+    @FXML // This method is called by the FXMLLoader when initialization is complete
+    void initialize() {
+        assert txtNome != null : "fx:id=\"txtNome\" was not injected: check your FXML file 'HelloMe.fxml'.";
+        assert txtResult != null : "fx:id=\"txtResult\" was not injected: check your FXML file 'HelloMe.fxml'.";
+
+    }
+}
